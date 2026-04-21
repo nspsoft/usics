84d621ab5d110028420cc175d8dd5ed8f0de7aa2
NSP Software <nspsoft.info@gmail.com>
2026-04-21 16:29:25 +0700

    Safety revert: commenting out new scheduled command to debug 500 error

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ use App\Jobs\ScheduledBackupJob;
 
 Schedule::job(new ScheduledBackupJob)->dailyAt('00:00');
 Schedule::command('inventory:check-low-stock')->dailyAt('08:00');
-Schedule::command('purchase-order:fix-creator')->hourly();
+// Schedule::command('purchase-order:fix-creator')->hourly();
