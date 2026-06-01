a04cda602e3118eec9481f4f543df1a659f31424
NSP Software <nspsoft.info@gmail.com>
2026-06-01 21:34:38 +0700

    fix(manufacturing): work order template route

--- routes/web.php
+++ routes/web.php
@@ -403,9 +403,9 @@ Route::prefix('manufacturing')->name('manufacturing.')->middleware(['auth'])->gr
     Route::post('/boms/{bom}/activate', [BomController::class, 'activate'])->name('boms.activate');
     Route::post('/boms/{bom}/archive', [BomController::class, 'archive'])->name('boms.archive');
 
-    Route::resource('work-orders', WorkOrderController::class);
     Route::get('/work-orders/template', [WorkOrderController::class, 'template'])->name('work-orders.template');
     Route::post('/work-orders/import', [WorkOrderController::class, 'import'])->name('work-orders.import');
+    Route::resource('work-orders', WorkOrderController::class);
     Route::post('/work-orders/{workOrder}/confirm', [WorkOrderController::class, 'confirm'])->name('work-orders.confirm');
     Route::post('/work-orders/{workOrder}/revert-to-draft', [WorkOrderController::class, 'revertToDraft'])->name('work-orders.revert-to-draft');
     Route::post('/work-orders/{workOrder}/start', [WorkOrderController::class, 'start'])->name('work-orders.start');
