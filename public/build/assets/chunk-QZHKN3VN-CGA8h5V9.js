714bcbeaf135d70f8600a2ec41c162083f796101
NSP Software <nspsoft.info@gmail.com>
2026-07-02 22:32:12 +0700

    feat(security-gate): implement RFID Security Gate Dashboard for pos satpam with photo previews, document status checks, and USB RFID keyboard simulation

---
+++ public/build/assets/chunk-QZHKN3VN-CGA8h5V9.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./Index-Iy62D5ZY.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as I};
